9cc7f43b736b2c0f910cc2e4eacf7e837ca54391
Jorge Rivas <jorge2233@hotmail.es>
2018-11-14 21:24:45 +0100

    Se añaden nuevas sentencias sql relacionadas con la creación de bases de datos y tablas

--- dml.sql
+++ dml.sql
@@ -17,3 +17,22 @@ mysql> SELECT
     -> USER()
     -> \c
 mysql>
+-- (crear un usuario)
+mysql> GRANT ALL ON nombre_bd.* TO 'nombre_usuario'@'dominio';
+-- (crear base datos)
+mysql> CREATE DATABASE nombre_bd;
+-- ()
+SHOW TABLES;
+-- (mostar el contenido de la tabla)
+SELECT * FROM nombre_tabla
+-- (mostrar el contenido de parte (user en mayúsculas es el nombre de la tabla))
+SELECT user,host FROM USER
+-- (nos muestra la estructura de la tabla)
+describe nombre_tabla
+-- (crear tabla)
+CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20),
+species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+-- ()
+INSERT INTO pet
+VALUES ('Puffball','Diane','hamster','f','1999-03-30',NULL);
+
