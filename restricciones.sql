5b09f1a6cb583b1a5731daeb45b100dec22a145b
Jorge Rivas <jorge2233@hotmail.es>
2018-11-21 21:25:29 +0100

    Nuevas restricciones

--- restricciones.sql
+++ restricciones.sql
@@ -28,3 +28,35 @@ INSERT INTO pelis2 (nombre, id, fecha_estreno) VALUES
 ('predators', '0012143M', '2010/03/07'), 
 ('alien', '0013223M', '1980/08/07'), 
 ('alien_vs_predator', '9992133L', '2003/01/06');
+
+
+-- (usamos auto_increment)
+CREATE TABLE proveedores (código int auto_increment, nombre varchar (20), dirección varchar(50), pago float, primary key(código));
+-- (tras usar auto_increment no necesitamos indicar el código)
+insert into proveedores (nombre, dirección, pago) values ('Pablo', 'paseo fueros de aragón', '30'), ('Mario', 'paseo constitución', '90');
+-- (comprobamos)
+select * from proveedores;
++---------+--------+-------------------------+------+
+| código  | nombre | dirección               | pago |
++---------+--------+-------------------------+------+
+|       1 | Pablo  | paseo fueros de aragón  |   30 |
+|       2 | Mario  | paseo constitución      |   90 |
++---------+--------+-------------------------+------+
+
+-- (tercera restricción: foreign key, debe haber el mismo valor en las 2 tablas)
+create table productos (código_p int auto_increment, codigo_provee int, nombre varchar(40), precio float, disponible int, primary key(código_p), foreign key(codigo_provee) references proveedores (código));
+-- (usamos esto para que se actualice o elimine en la tabla a la que hace referencia)
+create table productos2 (código_p int auto_increment, codigo_provee int, nombre varchar(40), precio float, disponible int, primary key(código_p), foreign key(codigo_provee) references proveedores (código) ON DELETE CASCADE ON UPDATE CASCADE);
+-- (eliminar claves primarias y volverla a añadir)
+show create table productos;
+-- ((seguimos) nos muestra algo así: CONSTRAINT `productos_ibfk_1`) 
+alter table productos drop foreign key productos_ibfk_1;
+alter table productos add foreign key (codigo_provee) references proveedores (código) on delete cascade on update cascade;
+-- (eliminar todos los registros de 1 tabla)
+truncate table productos3;
+-- (eliminar una tabla)
+drop table productos3;
+-- (para evitar valores nulos, es decir que no te deje introducirlos)
+nombre varchar (20) not null;
+
+
